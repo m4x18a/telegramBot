19ffccc409b0823df818f01af1cc5f354536f534
Maxim Babukhin <m4x18a@gmail.com>
2021-10-30 11:40:15 +0300

    Version 1

--- bot.py
+++ bot.py
@@ -1,29 +1,19 @@
-import logging
+import telebot
+import configure
+from googletrans import Translator
 
-from aiogram import Bot, Dispatcher, executor, types
+bot = telebot.TeleBot(configure.config['token'])
 
-API_TOKEN = '2079048524:AAHUkOPfpdnjtjntvZ-a8iIgfxgb-ArUAxo'
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hi! I'm bot-translator. Write any text, you want to translate.")
 
-# Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
 
-dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Привет!\nЯ t-bot!")
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    translator = Translator()
+    result = translator.translate(message.text)
+    bot.send_message(message.chat.id, result.text)
 
-@dp.message_handler(content_types=['text'])
-async def get_text_messages(msg: types.Message):
-   if msg.text.lower() == 'привет':
-       await msg.answer('Привет!')
-   else:
-       await msg.answer('Не понимаю, что это значит.')
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+bot.infinity_polling()
---
+++ configure.py
@@ -0,0 +1,4 @@
+config = {
+    'name': 'm4x18a_bot',
+    'token': '2079048524:AAF_7PhpwpsHGQzpRT2sEnX8q2sZKdaHcAc'
+}
